7546481644535cd965b11f1b734964d39c877142
james.o.osborne@gmail.com <6NqC@1gKzvesuV;esxBh>
2025-05-18 14:11:50 +1000

    commit test

--- PasswordStrengthChecker.py
+++ PasswordStrengthChecker.py
@@ -1,6 +1,5 @@
 def password_strength(password):
     #Password strength score and feedback
-    #test commit line
     strength_score = 0
     feedback = []
     #Check password length- 8 char minimum
